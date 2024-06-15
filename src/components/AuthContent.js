5c281c14dd6e1b1220a1ccaab0cf81b6f7ee3080
DanieleCiucci <daniele.ciucci96@gmail.com>
2024-06-15 11:03:22 +0200

    remove unused log

--- src/components/AuthContent.js
+++ src/components/AuthContent.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { request } from '../axios_helper';
 import logoBackground from '../assets/logoBackgroud.svg';
 import logo from '../assets/appLogo.svg';
@@ -12,22 +12,16 @@ export default class AuthContent extends React.Component {
         super(props);
         this.state = {
             data: [],
-            isAuthenticated: true
+            isAuthenticated: true  // You might want to initialize this based on actual authentication status
         };
     }
 
-    logState() {
-        console.log('Current state:', this.state);
-        console.log('User details:', this.props.user);  // Log user details from props
-    }
-
     componentDidMount() {
-        this.logState(); // Log state after mounting
         if (this.state.isAuthenticated) {
             request("GET", "/messages", {})
                 .then((response) => {
                     if (Array.isArray(response.data)) {
-                        this.setState({ data: response.data }, () => this.logState()); // Log state after setting data
+                        this.setState({ data: response.data });
                     } else {
                         console.error("Response data is not an array:", response.data);
                     }
@@ -41,61 +35,40 @@ export default class AuthContent extends React.Component {
     }
 
     render() {
-        const { logout, user } = this.props;  // Destructure user from props
-        const buttonText = user && user.role === 0 ? "YOUR APPOINTMENT" : "SCHEDULE APPOINTMENT"; // Determine button text
+        const { logout, user } = this.props;
+        const buttonText = user && user.role === 0 ? "YOUR APPOINTMENT" : "SCHEDULE APPOINTMENT";
 
         return (
             <div className="AuthHome">
-                <AuthHeader logout={logout} /> {/* Pass the logout function here */}
-                <img src={logoBackground} alt="Logo Background"
-                     style={{position: 'absolute', top: '0', left: '0', zIndex: '100', height: '6rem', width: 'auto'}}/>
-                <img src={logo} alt="App Logo"
-                     style={{position: 'absolute', top: '1rem', left: '1rem', zIndex: '100', height: '3rem', width: 'auto'
-                     }}/>
+                <AuthHeader logout={logout} />
+                <img src={logoBackground} alt="Logo Background" style={{ position: 'absolute', top: '0', left: '0', zIndex: '100', height: '6rem', width: 'auto' }} />
+                <img src={logo} alt="App Logo" style={{ position: 'absolute', top: '1rem', left: '1rem', zIndex: '100', height: '3rem', width: 'auto' }} />
 
                 <div className="row justify-content-md-center mt-5">
                     <div className="row mt-5">
-                        <div className="col-12 d-flex flex-column align-items-center"
-                             style={{marginTop: 'rem', textAlign: 'center'}}>
-                            <h1 style={{fontWeight: 'bold'}}>
-                                "Welcome to PawPal!<br/>
-                                Find the dog sitter for <br/> your furry friend.”
+                        <div className="col-12 d-flex flex-column align-items-center" style={{ marginTop: 'rem', textAlign: 'center' }}>
+                            <h1 style={{ fontWeight: 'bold' }}>
+                                "Welcome to PawPal!<br />
+                                Find the dog sitter for <br /> your furry friend.”
                             </h1>
                             <div className="mt-3">
-                                <p style={{fontSize: '1.5rem'}}>Because every dog deserves <br/> a new best friend.</p>
-                                <button className="homeButton">{buttonText}</button> {/* Render button text based on user role */}
+                                <p style={{ fontSize: '1.5rem' }}>Because every dog deserves <br /> a new best friend.</p>
+                                <button className="homeButton">{buttonText}</button>
                             </div>
                         </div>
                     </div>
-
-                    {/*
-                    <div className="card" style={{ width: "18rem" }}>
-                        <div className="card-body">
-                            <h5 className="card-title">Back end response</h5>
-                            <p className="card-text">content:</p>
-                            <ul>
-                                {this.state.data.length > 0 ? (
-                                    this.state.data.map((line, index) => (
-                                        <li key={index}>{line}</li>
-                                    ))
-                                ) : (
-                                    <p>No messages available or user not authenticated.</p>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                    */}
                 </div>
+
                 <div className="footer">
                     <div className="row mt-5">
                         <div className="col-3 px-md-5">
-                            <img src={dogFooter2} alt="Dog Footer" className="footerHomeDog2img"/>
+                            <img src={dogFooter2} alt="Dog Footer" className="footerHomeDog2img" />
                         </div>
                         <div className="col-6 align-content-center">
                         </div>
                         <div className="col-3 d-flex flex-row-reverse bd-highlight ">
-                            <img src={dogFooter} alt="Dog Footer" className="footerHomeDog1img"/>
-                            <img src={backGroudFooterImage} alt="Dog Footer" className="footerBackGroudLogo"/>
+                            <img src={dogFooter} alt="Dog Footer" className="footerHomeDog1img" />
+                            <img src={backGroudFooterImage} alt="Dog Footer" className="footerBackGroudLogo" />
                         </div>
                     </div>
                 </div>
