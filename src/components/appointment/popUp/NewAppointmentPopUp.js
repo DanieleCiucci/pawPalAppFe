645b14a3507daf64c4a06ccbb5f59a0c10ba6cae
dciucci <daniele.ciucci96@gmail.com>
2024-08-11 22:15:44 +0200

    add the paginator

--- src/components/appointment/popUp/NewAppointmentPopUp.js
+++ src/components/appointment/popUp/NewAppointmentPopUp.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import imageCard from "../../../assets/dog1.jpg";
 import { scheduleAppointment } from '../service/AppointmentService';
+import Pagination from "../../Paginator";
 
 const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
     const [appointmentData, setAppointmentData] = useState({
@@ -14,6 +15,8 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
     const [alert, setAlert] = useState(null);
     const [dogs, setDogs] = useState([]);
     const [serviceType, setServiceType] = useState('');
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(0);
 
     const serviceOptions = [
         { value: '', label: "Any" },
@@ -29,7 +32,7 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
             const fetchDogs = async () => {
                 try {
                     const token = localStorage.getItem('authToken');
-                    const response = await fetch("http://localhost:8080/api/dog/all-dog-owner", {
+                    const response = await fetch(`http://localhost:8080/api/dog/all-dog-owner?page=${currentPage}&size=6`, {
                         method: "GET",
                         headers: {
                             "Content-Type": "application/json",
@@ -40,7 +43,9 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
                         throw new Error('Network response was not ok');
                     }
                     const data = await response.json();
-                    setDogs(data.content || []); // Assuming 'content' is the key in the API response
+                    setDogs(data.content || []);
+                    setTotalPages(data.totalPages);
+
                 } catch (error) {
                     console.error("Error fetching dogs:", error);
                     setAlert({ type: 'danger', message: 'Error fetching dog data.' });
@@ -48,7 +53,11 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
             };
             fetchDogs();
         }
-    }, [show]);
+    }, [show, currentPage]);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -81,7 +90,6 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
         }
     };
 
-
     const handleServiceTypeChange = (event) => {
         const serviceId = event.target.value;
         setServiceType(serviceId);
@@ -90,7 +98,6 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
             serviceId: serviceId
         }));
     };
-
 
     const handleDateTimeChangeCheckin = (event) => {
         setAppointmentData(prevData => ({
@@ -194,41 +201,52 @@ const NewAppointmentPopUp = ({ show, handleClose, sitterId }) => {
                     <p><strong>Dogs involved in the appointment</strong></p>
                     <div className="col-md-12 mb-3">
                         {dogs.length > 0 ? (
-                            <div className="row mt-3">
-                                {dogs.map((dog) => (
-                                    <div key={dog.idDog} className="col-md-6 mb-3">
-                                        <div className="d-flex align-items-center border p-2"
-                                             style={{borderRadius: '14px', width: '100%'}}>
-                                            <img
-                                                src={dog.image ? `data:image/jpeg;base64,${dog.image}` : imageCard}
-                                                alt={dog.dogName}
-                                                style={{
-                                                    width: '50px',
-                                                    height: '50px',
-                                                    borderRadius: '50%',
-                                                    marginRight: '10px'
-                                                }}
-                                            />
-                                            <div className="d-flex flex-column flex-grow-1 justify-content-center mb-3">
-                                                <strong>{dog.dogName}</strong>
-                                                <p className="mb-0 text-muted"
-                                                   style={{fontSize: '14px'}}>{dog.dogBreeds}</p>
-                                            </div>
-                                            <div className="form-check">
-                                                <input
-                                                    className="form-check-input"
-                                                    type="checkbox"
-                                                    id={`dogCheck-${dog.idDog}`}
-                                                    checked={appointmentData.dogIds.includes(dog.idDog)}
-                                                    onChange={() => handleDogSelectionChange(dog.idDog)}
+                            <>
+                                <div className="row mt-3">
+                                    {dogs.map((dog) => (
+                                        <div key={dog.idDog} className="col-md-6 mb-3">
+                                            <div className="d-flex align-items-center border p-2"
+                                                 style={{borderRadius: '14px', width: '100%'}}>
+                                                <img
+                                                    src={dog.image ? `data:image/jpeg;base64,${dog.image}` : imageCard}
+                                                    alt={dog.dogName}
+                                                    style={{
+                                                        width: '50px',
+                                                        height: '50px',
+                                                        borderRadius: '50%',
+                                                        marginRight: '10px'
+                                                    }}
                                                 />
-                                                <label className="form-check-label"
-                                                       htmlFor={`dogCheck-${dog.idDog}`}></label>
+                                                <div className="d-flex flex-column flex-grow-1 justify-content-center mb-3">
+                                                    <strong>{dog.dogName}</strong>
+                                                    <p className="mb-0 text-muted"
+                                                       style={{fontSize: '14px'}}>{dog.dogBreeds}</p>
+                                                </div>
+                                                <div className="form-check">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        id={`dogCheck-${dog.idDog}`}
+                                                        checked={appointmentData.dogIds.includes(dog.idDog)}
+                                                        onChange={() => handleDogSelectionChange(dog.idDog)}
+                                                    />
+                                                    <label className="form-check-label"
+                                                           htmlFor={`dogCheck-${dog.idDog}`}></label>
+                                                </div>
                                             </div>
                                         </div>
+                                    ))}
+                                </div>
+                                {totalPages > 1 && (
+                                    <div className="d-flex justify-content-end mt-4 mb-4">
+                                        <Pagination
+                                            currentPage={currentPage}
+                                            totalPages={totalPages}
+                                            onPageChange={handlePageChange}
+                                        />
                                     </div>
-                                ))}
-                            </div>
+                                )}
+                            </>
                         ) : (
                             <p>No dogs available</p>
                         )}
