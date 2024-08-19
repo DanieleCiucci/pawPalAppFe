ea678f6a974019cea610be0b713d0c2d2820baba
DanieleCiucci <daniele.ciucci96@gmail.com>
2024-08-19 09:01:27 +0200

    production url on login

--- src/axios_helper.js
+++ src/axios_helper.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL ='http://localhost:8080';
+const apiUrl = process.env.REACT_APP_API_URL;
+
+axios.defaults.baseURL =apiUrl;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 
