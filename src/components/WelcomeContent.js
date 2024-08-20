29897aa536827e96fdbb3d6641654a26e0df6a09
DanieleCiucci <daniele.ciucci96@gmail.com>
2024-08-20 08:50:22 +0200

    first commit home page

--- src/components/WelcomeContent.js
+++ src/components/WelcomeContent.js
@@ -1,6 +1,9 @@
 
 import * as React from "react";
 
+import mainImage from '../assets/mainImageHome.svg'
+
+
 export default class WelcomeContent extends React.Component {
 
     render() {
@@ -8,8 +11,26 @@ export default class WelcomeContent extends React.Component {
             <div className="row justify-content-md-center">
                 <div className="jumbotron jumbotron-fluid">
                     <div className="container">
-                        <h1 className="display-4">Welcome</h1>
-                        <p className="lead">Log in to see protected content</p>
+
+                        <div className="row mainLoginBg p-5 m-5">
+
+                            <div className="col-6">
+                                <h1>
+                                    Welcome back <br /> to PawPal
+                                </h1>
+                                <p> Please enter your details.</p>
+                            </div>
+
+
+                            <div className="col-6">
+                                <img src={mainImage} alt="Background Footer" className="footerBackGroudLogo" />
+                            </div>
+
+                        </div>
+
+
+
+
                     </div>
                 </div>
             </div>
